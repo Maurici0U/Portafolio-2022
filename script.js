d3eb3f21095ef01331c277c679ed6d793518cf45
Maurici0U <92276414+Maurici0U@users.noreply.github.com>
2022-06-09 18:22:11 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ $(document).ready(function(){
         autoplayHoverPause: true,
         responsive: {
             0:{
-                items: 1,
+                items: 2,
                 nav: false
             
             }
